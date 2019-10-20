9d74d400ae748757e2b9a197afe8c3c3a04138ad
judC <hycai@edu.uwaterloo.ca>
2019-10-20 01:02:21 -0400

    Remove

--- internal/scanner.go
+++
@@ -1,9 +0,0 @@
-package internal
-
-type commentScanner struct {
-	fileName            string // name of file
-	line                int    // total number of lines of file
-	totalComments       int    // total number of comments
-	totalSingleComments int    // total number of single line comments
-	totalMultiComments  int    // total number of multi line comments
-}
