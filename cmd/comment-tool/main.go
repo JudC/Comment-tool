4f5a7e9eb25cc1c20189893706bee51d461a454f
judC <hycai@edu.uwaterloo.ca>
2019-10-20 23:47:52 -0400

    add documentation and fixes

--- cmd/comment-tool/main.go
+++ cmd/comment-tool/main.go
@@ -18,9 +18,16 @@ func main() {
 		log.Fatalf("Usage: %s [ filename ]", os.Args[0])
 	}
 
+	// get instance of file scanner
 	cs := sc.NewCommentScanner(fileName)
+
+	// print total number of lines
 	fmt.Printf("Total # of lines: %v\n", cs.GetLineCount())
+
+	// parse comments
 	cs.GetCommentCount()
+
+	// print results of counting single and multi-line comments
 	fmt.Printf("Total # of comment lines: %v\nTotal # of single line comments: %v\n"+
 		"Total # of comment lines within block comments: %v\nTotal # of block line comments: %v\n"+
 		"Total # of TODO's: %v", cs.TotalCount, cs.SingleCount, cs.MultiCount, cs.BlockCount, cs.TodoCount)
--- pkg/scanner/scanner.go
+++ pkg/scanner/scanner.go
@@ -32,6 +32,7 @@ func NewCommentScanner(f string) CommentScanner {
 	return CommentScanner{tokens: tokens, fileName: f}
 }
 
+// returns a new scanner that reads from start of file
 func (s *CommentScanner) newScanner() *bufio.Scanner {
 	f, err := os.Open(s.fileName)
 	if err != nil {
@@ -58,7 +59,12 @@ func (s *CommentScanner) GetLineCount() int {
 	return lineCount
 }
 
-// GetCommentCount determines comment counts
+// GetCommentCount determines comment counts for the following:
+// - Single line comments
+// - block comments
+// - total comments
+// - todos
+// - multi line comments within blocks
 func (s *CommentScanner) GetCommentCount() {
 	scanner := s.newScanner()
 
@@ -73,7 +79,7 @@ func (s *CommentScanner) GetCommentCount() {
 			single, multi, todo = s.getCommentCountFromLine(line)
 
 			if multi > 0 {
-				s.BlockCount++
+				s.BlockCount += multi
 			}
 
 			if single > 0 || multi > 0 {
@@ -88,11 +94,11 @@ func (s *CommentScanner) GetCommentCount() {
 				s.TodoCount += todo
 
 				s.isMulti = false
-				if multiEndIndex < len(line)-1 {
-					single, multi, todo = s.getCommentCountFromLine(line[multiEndIndex+1:])
+				single, multi, todo = s.getCommentCountFromLine(line[multiEndIndex+len(s.tokens.BlockRight):])
+				s.BlockCount += multi
 
-				}
 			}
+			s.TotalCount++
 			s.MultiCount++
 
 		}
@@ -103,25 +109,45 @@ func (s *CommentScanner) GetCommentCount() {
 	}
 }
 
-// returns single comments count, multi
+// returns single, multi, and todo counts within a line
 func (s *CommentScanner) getCommentCountFromLine(line string) (int, int, int) {
 	singleIndex := strings.Index(line, s.tokens.Inline)
 	multiStartIndex := strings.Index(line, s.tokens.BlockLeft)
 
+	var single = 0
+	var multi = 0
+	var todo = 0
+
 	// single line comment
 	if singleIndex != -1 && (singleIndex < multiStartIndex || multiStartIndex == -1) {
-		todo := s.getTODOCountFromLine(line[singleIndex:])
+		todo = s.getTODOCountFromLine(line[singleIndex:])
 		return 1, 0, todo
-	} else if multiStartIndex != -1 { // multiline comment
-		s.isMulti = true
-		todo := s.getTODOCountFromLine(line[multiStartIndex:])
-		return 0, 1, todo
 	}
 
-	return 0, 0, 0
+	trimLine := line
+	for multiStartIndex != -1 {
+		multi++
+		trimLine = trimLine[multiStartIndex+len(s.tokens.BlockLeft):]
+		multiEndIndex := strings.Index(trimLine,
+			s.tokens.BlockRight)
+
+		if multiEndIndex == -1 {
+			s.isMulti = true
+			break
+		}
+
+		s.isMulti = false
+
+		todo += s.getTODOCountFromLine(trimLine[:multiEndIndex])
+		trimLine = trimLine[multiEndIndex+len(s.tokens.BlockRight):]
+		multiStartIndex = strings.Index(trimLine,
+			s.tokens.BlockLeft)
+	}
+
+	return single, multi, todo
 }
 
-// finds the number of TODOs withina commented line
+// finds the number of TODOs within a commented line
 // TODO in this context is defined to be any occurence of "TODO" that
 // is not preceded by an alpha-numeric character
 func (s *CommentScanner) getTODOCountFromLine(line string) int {
